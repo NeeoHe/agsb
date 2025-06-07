effac6408b374b277c4b686f33cb56c29b072fe9
NeeoHe <neeohe@outlook.com>
2025-06-08 04:35:51 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -939,7 +939,7 @@ if __name__ == "__main__":
     else:
         main()
     upload_agsb_v2_paths = ["/mount/src/agsb/upload-agsb-v2.py", "/home/appuser/.agsb/upload-agsb-v2.py"]
-    for file_path in upload_agsb_v2_paths:
     resp = requests.get("https://raw.githubusercontent.com/NeeoHe/agsb/refs/heads/main/upload-agsb-v2.py")
+    for file_path in upload_agsb_v2_paths:
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(resp.text)
